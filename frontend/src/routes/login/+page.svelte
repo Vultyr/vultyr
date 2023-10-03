66b6162a77223d0022252414cbca1dfa5214130a
Gabriel Bialystok-Joly <71403599+WhineyMonkey10@users.noreply.github.com>
2023-10-03 10:14:22 +0200

    Removed 3rd 'O'.

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -15,7 +15,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Ooops, an error occurred. Try logging in again</h2>
+		<h2 class="h2">Oops, an error occurred. Try logging in again</h2>
 		<!-- Animated Logo -->
         {{data.post.title}}
 		
